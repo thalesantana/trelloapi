64c2cea6f54fb16d00fc43fc25e81e53c865b8a6
Thales <thales.c.santana@outlook.com>
2021-03-31 12:15:49 -0300

    create comment functional

--- src/pages/api/createCards/index.ts
+++ src/pages/api/createCards/index.ts
@@ -8,7 +8,7 @@ export default async function(req: NextApiRequest, res: NextApiResponse){
       const {method} = req;
       switch (method) {
         case 'POST':
-          const {name, idList,email} = req.body;
+          const {name, idList,email,text} = req.body;
           fetch(`https://api.trello.com/1/cards?key=73af314f1d4579f0ffe6c3604190583c&token=22edae9d846384150bd3822e9067927b8e643c16ee09aa153efc167ad09b3aa2&idList=${idList}&name=${name}&email=${email}`, {
               method: 'POST'
               })
@@ -16,9 +16,11 @@ export default async function(req: NextApiRequest, res: NextApiResponse){
                   return response.json();
               })
               .then((data) => {
-                  //console.log(data)
-                  resolve(res.status(200).json(data))
+                  fetch( `https://api.trello.com/1/cards/${data.id}/actions/comments?key=73af314f1d4579f0ffe6c3604190583c&token=22edae9d846384150bd3822e9067927b8e643c16ee09aa153efc167ad09b3aa2&text=${text}`,{
+                    method: 'POST'
                   })
+                  resolve(res.status(200).json(data))
+                  })    
               .catch(err =>{ 
                   console.error(err)
                   return res.status(400).send(err)
